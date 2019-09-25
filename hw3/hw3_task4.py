2346a4b01845a9e722d3d1e5c533464cec0da84c
itsanti <kurov.aleksandr+github@gmail.com>
2019-09-26 00:43:23 +0300

    HW3: task 4

---
+++ hw3/hw3_task4.py
@@ -0,0 +1,31 @@
+"""HW3 - Task 4
+
+Определить, какое число в массиве встречается чаще всего.
+"""
+
+import random
+
+in_arr = [random.randint(-3, 3) for _ in range(3)]
+print(in_arr)
+
+d = {}
+count = 0
+for el in in_arr:
+    d[el] = d.get(el, 0) + 1
+    count = d[el] if d[el] > count else count
+
+if count > 1:
+    for k, val in d.items():
+        if val == count:
+            print(f'число {k} встречается чаще всего')
+else:
+    print('все числа встречаются 1 раз')
+
+'''OUTPUT
+[0, 2, 3, -2, -1, 2, 2, -1, -1, 3]
+число 2 встречается чаще всего
+число -1 встречается чаще всего
+
+[3, 1, -1, 2, 3, -2, -2, 3, 1, 0]
+число 3 встречается чаще всего
+'''
